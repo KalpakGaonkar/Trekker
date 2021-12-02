94065706deddb8a526bde5a144a5c086e59698b1
kalpakgaonkar <kalpakgaonkar19@gmail.com>
2021-12-02 15:10:41 -0500

    Initial Commit

---
+++ Trekker/TrekkerApp.swift
@@ -0,0 +1,19 @@
+//
+//  TrekkerApp.swift
+//  Trekker
+//
+//  Created by Kalpak Gaonkar on 12/1/21.
+//
+
+import SwiftUI
+
+@main
+struct TrekkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView{
+            ContentView()
+            }
+        }
+    }
+}
---
+++ Trekker/location.swift
@@ -0,0 +1,8 @@
+//
+//  location.swift
+//  Trekker
+//
+//  Created by Kalpak Gaonkar on 12/2/21.
+//
+
+import Foundation
